c81a23a88049c73af3a87ab4005e76f0e632f884
Macbook Pro <macbookpro@Macbooks-MBP-3.localdomain>
2025-08-12 16:57:25 -0400

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,14 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import Bloglist from './Bloglist';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     
-
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div>}
+            {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
         </div>
     );
         
